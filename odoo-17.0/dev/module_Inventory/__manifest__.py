c4c80a1183e3b72fcd3520befe281cd8b2cc2284
PanitanK <98577467+PanitanK@users.noreply.github.com>
2024-05-15 09:26:18 +0700

    15/5

---
+++ odoo-17.0/dev/module_Inventory/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'FishInventory',
+    'author': 'Panitan K',
+    'version': '1.2',
+    'summary': 'Fish Inventory',
+    'data': [
+        'views/menu.xml',
+        'views/fish.xml',
+    ]
+}
---
+++ odoo-17.0/dev/module_Inventory/models/fish.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+
+class Fish(models.Model):
+    _name = 'module_inventory.fish'
+    _description = 'Test Fish stock'
+
+    order = fields.Integer(string="ลำดับที่")
+    product_name = fields.Char(string='ชื่อสินค้า')
+    description = fields.Char(string='คำอธิบายสินค้า')
+    protein_percent = fields.Float(string='โปรตีน(%)')
